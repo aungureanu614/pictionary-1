ca1b8d5a89b5d2053e69093ef2adc2739a2dc8d0
tstuart <tstuart@beckshybrids.com>
2015-06-01 20:39:04 -0400

    start of adding game engine

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
+var engine = require('./game.js');
+
+var gameEngine = new engine.GameEngine();
 
 var app = express();
 app.use(express.static('public'));
@@ -9,21 +12,16 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-// when a use enters the game mid stream, their
-// canvas will be blank.  To adjust, lets keep
-// a list of positions that are drawn on the
-// drawer's canvas between clears.
-var drawList = [];
 
 io.on('connection', function(socket) {
 
     // send any new connections content
     // of drawList so they can catch up
-    socket.emit('connected', drawList);
+    socket.emit('connected', gameEngine.drawList);
 
     socket.on('draw', function(position) {
         // log this for users that enter game and emit to connected users
-        drawList.push(position);
+        gameEngine.addDrawPosition(position);
         socket.broadcast.emit('draw', position);
     });
 
@@ -35,7 +33,7 @@ io.on('connection', function(socket) {
     // clear list of positions drawn and send
     // clear to connected users.
     socket.on('clear', function() {
-        drawList = [];
+        gameEngine.clearDrawList();
         socket.broadcast.emit('clear');
     });
 
